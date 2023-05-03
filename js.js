0007f3edf5f775d4474b9b686e787c975da9bcd9
phanbadu <duphan03@gmail.com>
2023-05-04 00:13:59 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,17 @@
+var range = document.querySelector('.range')
+var process = document.querySelector('.process')
+var value = document.querySelector('.process span')
+
+function updateProcess(percent) {
+    process.style.width = `${percent}%`
+    value.innerHTML = `${percent}%`
+}
+
+range.addEventListener('mousemove', function(e) {
+    var processWidth = e.pageX - this.offsetLeft
+    var percent = processWidth / this.offsetWidth * 100
+    percent = Math.round(percent)
+    updateProcess(percent)
+})
+
+updateProcess(30)
