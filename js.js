2b5a83a7276fc0757a1690aacd59f68ed991cd04
phanbadu <119209610+phanbadu@users.noreply.github.com>
2023-05-06 00:48:40 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 var range = document.querySelector('.range')
 var process = document.querySelector('.process')
 var value = document.querySelector('.process span')
+var body = document.querySelector('body')
 
 function updateProcess(percent) {
     process.style.width = `${percent}%`
     value.innerHTML = `${percent}%`
+    body.style.background = `linear-gradient(to right, rgba(0,0,0,${percent/100}),rgba(0,0,0,${percent/100})), url(7.jpg) no-repeat center/cover`
 }
 
 range.addEventListener('mousemove', function(e) {
